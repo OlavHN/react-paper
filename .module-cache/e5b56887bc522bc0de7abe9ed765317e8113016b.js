e62f7eb314309408b59d6f89de2eb7c3ca0ced21
Jason Brown <iam@browniefed.com>
2014-11-23 08:06:30 -0800

    Ignore module caches.

--- .module-cache/e5b56887bc522bc0de7abe9ed765317e8113016b.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Header: require('./Header'),
-  HeaderPanel: require('./HeaderPanel'),
-  Button: require('./Button'),
-  Dialog: require('./Dialog'),
-  Switch: require('./Switch'),
-  Progress: require('./Progress'),
-  Slider: require('./Slider')
-};
